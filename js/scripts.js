a461267e31675efd59c5a88c42a944de9dfe2a14
connor abdelnoor & Patrick Fiorentino <student@epicodus.com>
2015-04-15 10:27:10 -0700

    Add withdraw and declined tests and method.

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,12 @@ var BankAccount = {
     balance: 0,
     deposit: function(amount){
         return this.balance += amount;
+    },
+    withdraw: function(amount){
+        if ((this.balance - amount) <= 0) {
+            return "declined"
+        } else {
+            return this.balance -= amount;
+        }
     }
-}
+};
