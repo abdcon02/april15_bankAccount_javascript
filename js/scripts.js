980e361884a1cc85e7e49ebd49aaa4c973aafdf8
connor abdelnoor & Patrick Fiorentino <student@epicodus.com>
2015-04-15 12:03:55 -0700

    Add forms for deposit and withdraw

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,37 @@ var BankAccount = {
         }
     }
 };
+
+$(function() {
+    $("form#create_account").submit(function(event) {
+        event.preventDefault();
+        var name = $("#account_name").val();
+        var userAccount = Object.create(BankAccount);
+        userAccount.name = name;
+        $("#account_name").val("");
+
+        $("#accounts").show();
+        $("#account_list").append("<li>" + name + "</li>");
+
+        $("#account_list").last().click(function() {
+            $('#create_account').hide();
+            $('#accounts').hide();
+            $('#current_account').show();
+            $('#current_account h3').text(userAccount.name + ": $" + userAccount.balance);
+        });
+        $("form#deposit_amount").submit(function(event) {
+            event.preventDefault();
+            var amount = parseInt($("#deposit").val());
+            userAccount.deposit(amount);
+            $('#current_account h3').text(userAccount.name + ": $" + userAccount.balance);
+            $("#deposit").val("");
+        });
+        $("form#withdraw_amount").submit(function(event) {
+            event.preventDefault();
+            var amount = parseInt($("#withdraw").val());
+            userAccount.withdraw(amount);
+            $('#current_account h3').text(userAccount.name + ": $" + userAccount.balance);
+            $("#withdraw").val("");
+        });
+    });
+});
