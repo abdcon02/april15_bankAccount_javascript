d3e3b9c7b6f4560858cf8fb14fb36a15af7fc8a4
connor abdelnoor & Patrick Fiorentino <student@epicodus.com>
2015-04-15 10:48:15 -0700

    Add overdraft protection muwahahha

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 var BankAccount = {
     balance: 0,
+    overdraft: false,
     deposit: function(amount){
         return this.balance += amount;
     },
     withdraw: function(amount){
-        if ((this.balance - amount) <= 0) {
-            return "declined"
+        if (this.overdraft === false) {
+            if ((this.balance - amount) <= 0) {
+                return "declined"
+            } else {
+                return this.balance -= amount;
+            }
         } else {
-            return this.balance -= amount;
+            this.balance -= amount;
+            return this.balance -= 50;
         }
     }
 };
